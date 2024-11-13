00df3d6708048c7e140e0da74106ff60b70fa472
SeUkKim <eleccookie@gmail.com>
2024-11-13 17:11:04 +0900

    modify timeout value

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -4,7 +4,7 @@ import path from 'path'
 describe('test move package', () => {
   const contractDir = path.resolve(__dirname, 'contract/simple')
   const builder = new MoveBuilder(contractDir, {})
-  jest.setTimeout(20000)
+  jest.setTimeout(30000)
 
   it('executes tests successfully', async () => {
     const testResult = await builder.test()
