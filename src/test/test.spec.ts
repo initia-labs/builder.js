75b174492bf6228a57c2ca96ca880bd9b625eb4b
SeUkKim <eleccookie@gmail.com>
2024-11-13 17:08:23 +0900

    remove beforeAll

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -4,10 +4,7 @@ import path from 'path'
 describe('test move package', () => {
   const contractDir = path.resolve(__dirname, 'contract/simple')
   const builder = new MoveBuilder(contractDir, {})
-
-  beforeAll(() => {
-    jest.setTimeout(200000)
-  })
+  jest.setTimeout(20000)
 
   it('executes tests successfully', async () => {
     const testResult = await builder.test()
